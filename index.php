0738515377fa82694719a6933bbe73788f508dc8
ryougaueno <ryougaueno@gmail.com>
2020-02-17 21:19:05 +0800

    エラー出てます

---
+++ index.php
@@ -0,0 +1,26 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Document</title>
+    <link rel="stylesheet" href="main.css" type="text/css">
+</head>
+<body>
+    <!-- header -->
+    <header>
+    a
+    </header>
+    <!-- main -->
+    <main>
+
+    </main>
+<!-- footer -->
+    <footer>
+
+    </footer>
+</body>
+</html>
