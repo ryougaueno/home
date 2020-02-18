d23efe9e0276439461aa113e2cfb888dc18e408d
鈴木悠祐 <>
2020-02-18 11:59:19 +0800

    add

--- edit.php
+++ edit.php
@@ -1,5 +1,26 @@
 <?php
-test
+
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>edit.php</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/edit.css">
+
+</head>
+<body>
+    <h1>タイトル</h1>
+    <div>title</div>
+    <input type="text" name="view-title">
+    <div>content</div>
+    <input type="text" name="view-content">
+    <br>
+    <input type="submit" value="投稿">
+</body>
+</html>
