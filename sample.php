a52795526f6a7a82cbcfbdea5648e2ec75b21bf0
ryougaueno <ryougaueno@gmail.com>
2020-02-18 10:21:38 +0800

    db

--- sample.php
+++ sample.php
@@ -7,12 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <title>Document</title>
-    <link rel="stylesheet" href="main.css" type="text/css">
+    <link type="text/css" rel="stylesheet" href="main.css" >
+    
 </head>
 <body>
     <!-- header -->
     <header>
-    a
+    aA
     </header>
     <!-- main -->
     <main>
