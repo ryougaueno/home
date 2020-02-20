9bf496b43dd34fc5d1bf88213b764f9f408c9bb6
ryougaueno <ryougaueno@gmail.com>
2020-02-20 09:24:06 +0800

    Task.phpついか

--- Models/Task.php
+++ Models/Task.php
@@ -39,3 +39,43 @@ class Task extends Model
     }
     
 }
+
+
+class Comment extends Model
+{
+
+    // プロパティ
+    protected $comment = 'comment';
+
+
+    public function create($data)
+    {
+        // DBに保存
+        // このクラスのインスタンスの
+        // db_managerプロパティの
+        // DbManagerクラスのインスタンス
+        // dbhプロパティの
+        // PDOのインスタンス
+        // prepareメソッドを実行
+        // INSERT INTO (カラム名, ,) VALUES (値, 値, 値,)
+        $stmt = $this->db_manager->dbh->prepare('INSERT INTO ' . $this->table . ' (title, contents, created) VALUES (?, ?, ?)');
+        $stmt->execute($data);
+    }
+
+    public function update($data)
+    {
+        // データの更新
+        $stmt = $this->db_manager->dbh->prepare('UPDATE ' . $this->table . ' SET title = ?, contents = ? WHERE id = ?');
+        $stmt->execute($data);
+    }
+
+    public function findByTitle($data)
+    {
+        $stmt = $this->db_manager->dbh->prepare('SELECT * FROM ' . $this->table . ' WHERE title LIKE ?');
+        $stmt->execute($data);
+        $comment = $stmt->fetchAll();
+        // var_dump($data, $tasks);die;
+        return $comment;
+    }
+    
+}
